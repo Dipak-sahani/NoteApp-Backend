b40b6ea0c6e8982392dd219c4d9a8b2288496570
Dipak Sahani <sahanidipak671@gmail.com>
2025-09-15 23:04:13 +0530

    updating the db index . js

--- src/app.js
+++ src/app.js
@@ -8,9 +8,9 @@ const app = express()
 const server = createServer(app);
 
 const allowedOrigins = [ 
-  "https://notes-app-frontend-ryeq.vercel.app", // prod frontend (with https!)
+  "https://notes-app-frontend-ryeq.vercel.app/", // prod frontend (with https!)
   "http://localhost:5173",   // local dev (Vite default)
-  "http://localhost:3000"    // local dev (CRA default, if needed)
+  "https://notes-app-frontend-ryeq-git-main-dipak-sahanis-projects.vercel.app"    // local dev (CRA default, if needed)
 ];
 
 app.use(
