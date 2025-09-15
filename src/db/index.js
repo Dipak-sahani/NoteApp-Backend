69f4ac6fa9508a66dee7b9fb972cb5d17a1ad64b
Dipak Sahani <sahanidipak671@gmail.com>
2025-09-15 22:33:50 +0530

    updating the db index . js

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,23 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        let mongoUri = process.env.MONGODB_URI;
+
+        // Parse DB name presence in the URI
+        const uriHasDbName = /mongodb(\+srv)?:\/\/[^/]+\/[^/?]+/.test(mongoUri);
+
+        // Add DB_NAME if not present
+        let connectionString = uriHasDbName ? mongoUri : mongoUri.replace(/\/?$/, `/${DB_NAME}`);
+        //      ^ ensures there's exactly one slash before DB_NAME
+
+        const connectionInstance = await mongoose.connect(connectionString);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
+        process.exit(1);
     }
 }
 
-export default connectDB
+export default connectDB;
